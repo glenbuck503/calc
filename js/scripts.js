9ed34704f15eddb28e099279691317cfdd0429fa
glenbuck503 <glenbuck503@gmail.com>
2020-10-21 14:29:28 -0700

    add functions to calculator

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,29 @@
-function add(number1,number2) {
-  return number1+number2
-  }
-function subtract(number1, number2) {
-  return number1-number2
-  }  
+// Business Logic:
 
-function multiply(number1,number2) {
-  return number1 * number2
+function add(number1, number2) {
+  return number1 + number2;
 }
+
+function subtract(number1, number2) {
+  return number1 - number2;
+}
+
+function multiply(number1, number2) {
+  return number1 * number2;
+};
 
 function divide(number1, number2) {
-  return number1 / number2
-}
+  return number1 / number2;
+};
+  
+//Everything below this line is user interface logic:
 
-    
-  const number1=parseInt(prompt("Enter a number:"));
-  const number2=parseInt(prompt("Enter another number:"));
-  const result=divide(number1, number2);
-  alert(result);
-
+$(document).ready(function() {
+  $("form#add").submit (function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#add1").val());
+    const number2 = parseInt($("#add2").val());
+    const result = add(number1, number2);
+    $("#output").text(result);
+  });
+});
