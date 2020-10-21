7bad5309215b03fd617ab80571786794b04caec2
glenbuck503 <glenbuck503@gmail.com>
2020-10-21 15:19:32 -0700

    add more to calculator

--- js/scripts.js
+++ js/scripts.js
@@ -24,6 +24,30 @@ $(document).ready(function() {
     const number1 = parseInt($("#add1").val());
     const number2 = parseInt($("#add2").val());
     const result = add(number1, number2);
-    $("#output").text(result);
+    $("#output1").text(result);
+  });  
+
+  $("form#subtract").submit (function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#sub1").val());
+    const number2 = parseInt($("#sub2").val());
+    const result = subtract(number1, number2);
+    $("#output2").text(result);
+  });
+
+  $("form#multiply").submit (function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#multi1").val());
+    const number2 = parseInt($("#multi2").val());
+    const result = multiply(number1, number2);
+    $("#output3").text(result);
+  });
+
+  $("form#divide").submit (function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#divi1").val());
+    const number2 = parseInt($("#divi2").val());
+    const result = divide(number1, number2);
+    $("#output4").text(result);
   });
 });
